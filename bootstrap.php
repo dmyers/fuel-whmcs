081dc92bc4cc095ef7710bfb592d92ec50df449a
Derek Myers <dmyers@onesite.com>
2013-02-04 12:23:03 -0600

    Updated whmcs sdk

--- bootstrap.php
+++ bootstrap.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . DS . 'vendor' . DS . 'whmcs-sdk' . DS . 'autoloader.php';
 
-\Config::load('whmcs', true);
+Config::load('whmcs', true);
 
 $whmcs_url    = Config::get('whmcs.url');
 $api_url      = $whmcs_url . Config::get('whmcs.api_url');
